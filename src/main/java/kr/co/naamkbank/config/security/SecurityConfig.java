4e850949e84e00615100bcabfb546ace08553e7f
윤남경 <nk.yun@bpmg.biz>
2024-12-18 19:00:50 +0900

    feat : add to h2-console permit security config

---
+++ src/main/java/kr/co/naamkbank/config/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kr.co.naamkbank.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll() // H2 Console 접근 허용
+                        .anyRequest().authenticated() // 다른 요청은 인증 필요
+                )
+                .csrf(AbstractHttpConfigurer::disable) // H2 Console 사용 시 CSRF 비활성화
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // H2 Console의 iframe 사용 허용
+
+        return http.build();
+    }
+}
