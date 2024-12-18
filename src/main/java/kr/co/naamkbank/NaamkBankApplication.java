b818bfb39d827c0ccb6d62c0970543b91f8ea53f
윤남경 <nk.yun@bpmg.biz>
2024-12-18 15:54:33 +0900

    init

---
+++ src/main/java/kr/co/naamkbank/NaamkBankApplication.java
@@ -0,0 +1,13 @@
+package kr.co.naamkbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaamkBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaamkBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/naamkbank/NaamkBankApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.naamkbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaamkBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
