9a9ea445a440fe089babc332391a447732c37a50
윤남경 <nk.yun@bpmg.biz>
2024-12-30 14:10:49 +0900

    feat : apply APIResponseEntityBuilder at Permission Controller

--- src/main/java/kr/co/naamk/web/controller/PermController.java
+++ src/main/java/kr/co/naamk/web/controller/PermController.java
@@ -1,7 +1,10 @@
 package kr.co.naamk.web.controller;
 
 import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.naamk.exception.type.ServiceMessageType;
 import kr.co.naamk.web.dto.PermDto;
+import kr.co.naamk.web.dto.apiResponse.APIResponseEntityBuilder;
 import kr.co.naamk.web.service.PermService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +20,24 @@ public class PermController {
     private final PermService permService;
 
     @RequestMapping(value="", method = RequestMethod.POST)
-    public ResponseEntity<Object> createPermission(@RequestBody PermDto permDto) {
-        try {
-            permService.createPermission(permDto);
-            return ResponseEntity.ok("OK");
+    public Object createPermission(@RequestBody PermDto permDto,
+                                   HttpServletRequest request) {
+        permService.createPermission(permDto);
 
-        } catch(Exception e) {
-            throw e;
-        }
+        return APIResponseEntityBuilder.create().service(request)
+                .resultMessage(ServiceMessageType.SUCCESS)
+                .build();
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<Object> getPermissions(@RequestParam @Nullable Long permId) {
+    public Object getPermissions(@RequestParam @Nullable Long permId,
+                                 HttpServletRequest request) {
         List<PermDto.PermResponse> permissions = permService.getPermissions(permId);
 
-        return ResponseEntity.ok(permissions);
+        return APIResponseEntityBuilder.create().service(request)
+                .resultMessage(ServiceMessageType.SUCCESS)
+                .entity(permissions)
+                .build();
     }
 
 
